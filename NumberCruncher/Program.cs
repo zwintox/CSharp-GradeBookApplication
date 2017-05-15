87cdacbaf88680c4ecc9cacf4fecf39cbabf1ea3
Eric J Fisher <EricJFisher@Live.com>
2017-05-15 14:53:37 -0400

    Added starting Console App and Tests

---
+++ NumberCruncher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NumberCruncher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
