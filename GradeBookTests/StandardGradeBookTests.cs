e8c4521582e2b019e0b676650a2e9ebbd38b7594
Eric J Fisher <EricJFisher@Live.com>
2017-07-21 11:16:26 -0400

    Fixed all StandardGradeBookTests to compile when the project is not yet complete

--- GradeBookTests/StandardGradeBookTests.cs
+++ GradeBookTests/StandardGradeBookTests.cs
@@ -1,182 +1,130 @@
-﻿using Xunit;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Xunit;
 
+using GradeBook;
 using GradeBook.Enums;
-using GradeBook.GradeBooks;
 
 namespace GradeBookTests
 {
+    /* Disclaimer: The tests in this class are NOT recommended practice, they've been made far more compilated and fragile than standard tests out of the 
+     need for the project to be able to compile prior to student completion of all tasks which greatly complicates this process, you should not do this 
+     in real world testing. */
+
     public class StandardGradeBookTests
     {
         [Fact]
         public void ConstructorTest()
         {
-            var gradeBook = new StandardGradeBook("Test GradeBook", true);
-            Assert.True(gradeBook.Name == "Test GradeBook");
-            Assert.True(gradeBook.Type == GradeBookType.Standard);
-        }
+            var standardGradeBook = (from assembly in AppDomain.CurrentDomain.GetAssemblies()
+                                     from type in assembly.GetTypes()
+                                     where type.Name == "StandardGradeBook"
+                                     select type).FirstOrDefault();
+            if (standardGradeBook == null)
+                throw new Exception("GradeBook.GradeBooks.StandardGradeBook doesn't exist.");
 
-        [Fact]
-        public void GetGPAWeightedATest()
-        {
-            var gradeBook = new StandardGradeBook("Test GradeBook", true);
+            object gradeBook = Activator.CreateInstance(standardGradeBook, "Test GradeBook", true);
 
-            var expected = 5;
-            var actual = gradeBook.GetGPA('A',StudentType.Honors);
-            Assert.True(expected == actual);
-        }
+            var gradebookEnum = (from assembly in AppDomain.CurrentDomain.GetAssemblies()
+                                 from type in assembly.GetTypes()
+                                 where type.FullName == "GradeBook.Enums.GradeBookType"
+                                 select type).FirstOrDefault();
+            if (gradebookEnum == null)
+                throw new Exception("GradeBook.Enums.GradeBookType doesn't exist.");
 
-        [Fact]
-        public void GetGPAWeightedBTest()
-        {
-            var gradeBook = new StandardGradeBook("Test GradeBook", true);
-
-            var expected = 4;
-            var actual = gradeBook.GetGPA('B', StudentType.Honors);
-            Assert.True(expected == actual);
-        }
-
-        [Fact]
-        public void GetGPAWeightedCTest()
-        {
-            var gradeBook = new StandardGradeBook("Test GradeBook", true);
-
-            var expected = 3;
-            var actual = gradeBook.GetGPA('C', StudentType.Honors);
-            Assert.True(expected == actual);
-        }
-
-        [Fact]
-        public void GetGPAWeightedDTest()
-        {
-            var gradeBook = new StandardGradeBook("Test GradeBook", true);
-
-            var expected = 2;
-            var actual = gradeBook.GetGPA('D', StudentType.Honors);
-            Assert.True(expected == actual);
-        }
-
-        [Fact]
-        public void GetGPAWeightedFTest()
-        {
-            var gradeBook = new StandardGradeBook("Test GradeBook", true);
-
-            var expected = 1;
-            var actual = gradeBook.GetGPA('F', StudentType.Honors);
-            Assert.True(expected == actual);
-        }
-
-        [Fact]
-        public void GetGPAWeightedOtherTest()
-        {
-            var gradeBook = new StandardGradeBook("Test GradeBook", true);
-
-            var expected = 0;
-            var actual = gradeBook.GetGPA('E', StudentType.Honors);
-            Assert.True(expected == actual);
-        }
-
-        [Fact]
-        public void GetGPAUnweightedATest()
-        {
-            var gradeBook = new StandardGradeBook("Test GradeBook", false);
-
-            var expected = 4;
-            var actual = gradeBook.GetGPA('A', StudentType.Honors);
-            Assert.True(expected == actual);
-        }
-
-        [Fact]
-        public void GetGPAUnweightedBTest()
-        {
-            var gradeBook = new StandardGradeBook("Test GradeBook", false);
-
-            var expected = 3;
-            var actual = gradeBook.GetGPA('B', StudentType.Honors);
-            Assert.True(expected == actual);
-        }
-
-        [Fact]
-        public void GetGPAUnweightedCTest()
-        {
-            var gradeBook = new StandardGradeBook("Test GradeBook", false);
-
-            var expected = 2;
-            var actual = gradeBook.GetGPA('C', StudentType.Honors);
-            Assert.True(expected == actual);
-        }
-
-        [Fact]
-        public void GetGPAUnweightedDTest()
-        {
-            var gradeBook = new StandardGradeBook("Test GradeBook", false);
-
-            var expected = 1;
-            var actual = gradeBook.GetGPA('D', StudentType.Honors);
-            Assert.True(expected == actual);
-        }
-
-        [Fact]
-        public void GetGPAUnweightedFTest()
-        {
-            var gradeBook = new StandardGradeBook("Test GradeBook", false);
-
-            var expected = 0;
-            var actual = gradeBook.GetGPA('F', StudentType.Honors);
-            Assert.True(expected == actual);
-        }
-
-        [Fact]
-        public void GetGPAUnweightedOtherTest()
-        {
-            var gradeBook = new StandardGradeBook("Test GradeBook", false);
-
-            var expected = 0;
-            var actual = gradeBook.GetGPA('E', StudentType.Honors);
-            Assert.True(expected == actual);
+            Assert.True((string)gradeBook.GetType().GetProperty("Name").GetValue(gradeBook) == "Test GradeBook");
+            Assert.True(gradeBook.GetType().GetProperty("Type").GetValue(gradeBook).GetType() == Enum.Parse(gradebookEnum, "Standard", true).GetType());
         }
 
         [Fact]
         public void GetLetterGradeATest()
         {
-            var gradeBook = new StandardGradeBook("Test GradeBook", true);
+            var standardGradeBook = (from assembly in AppDomain.CurrentDomain.GetAssemblies()
+                                     from type in assembly.GetTypes()
+                                     where type.Name == "StandardGradeBook"
+                                     select type).FirstOrDefault();
+            if (standardGradeBook == null)
+                throw new Exception("GradeBook.GradeBooks.StandardGradeBook doesn't exist.");
+
+            object gradeBook = Activator.CreateInstance(standardGradeBook, "Test GradeBook", true);
+            MethodInfo method = standardGradeBook.GetMethod("GetLetterGrade");
+
             var expected = 'A';
-            var actual = gradeBook.GetLetterGrade(90);
+            var actual = (char)method.Invoke(gradeBook, new object[] { 90 });
             Assert.True(expected == actual);
         }
 
         [Fact]
         public void GetLetterGradeBTest()
         {
-            var gradeBook = new StandardGradeBook("Test GradeBook", true);
+            var standardGradeBook = (from assembly in AppDomain.CurrentDomain.GetAssemblies()
+                                     from type in assembly.GetTypes()
+                                     where type.Name == "StandardGradeBook"
+                                     select type).FirstOrDefault();
+            if (standardGradeBook == null)
+                throw new Exception("GradeBook.GradeBooks.StandardGradeBook doesn't exist.");
+
+            object gradeBook = Activator.CreateInstance(standardGradeBook, "Test GradeBook", true);
+            MethodInfo method = standardGradeBook.GetMethod("GetLetterGrade");
+
             var expected = 'B';
-            var actual = gradeBook.GetLetterGrade(80);
+            var actual = (char)method.Invoke(gradeBook, new object[] { 80 });
             Assert.True(expected == actual);
         }
 
         [Fact]
         public void GetLetterGradeCTest()
         {
-            var gradeBook = new StandardGradeBook("Test GradeBook", true);
+            var standardGradeBook = (from assembly in AppDomain.CurrentDomain.GetAssemblies()
+                                     from type in assembly.GetTypes()
+                                     where type.Name == "StandardGradeBook"
+                                     select type).FirstOrDefault();
+            if (standardGradeBook == null)
+                throw new Exception("GradeBook.GradeBooks.StandardGradeBook doesn't exist.");
+
+            object gradeBook = Activator.CreateInstance(standardGradeBook, "Test GradeBook", true);
+            MethodInfo method = standardGradeBook.GetMethod("GetLetterGrade");
+
             var expected = 'C';
-            var actual = gradeBook.GetLetterGrade(70);
+            var actual = (char)method.Invoke(gradeBook, new object[] { 70 });
             Assert.True(expected == actual);
         }
 
         [Fact]
         public void GetLetterGradeDTest()
         {
-            var gradeBook = new StandardGradeBook("Test GradeBook", true);
+            var standardGradeBook = (from assembly in AppDomain.CurrentDomain.GetAssemblies()
+                                     from type in assembly.GetTypes()
+                                     where type.Name == "StandardGradeBook"
+                                     select type).FirstOrDefault();
+            if (standardGradeBook == null)
+                throw new Exception("GradeBook.GradeBooks.StandardGradeBook doesn't exist.");
+
+            object gradeBook = Activator.CreateInstance(standardGradeBook, "Test GradeBook", true);
+            MethodInfo method = standardGradeBook.GetMethod("GetLetterGrade");
+
             var expected = 'D';
-            var actual = gradeBook.GetLetterGrade(60);
+            var actual = (char)method.Invoke(gradeBook, new object[] { 60 });
             Assert.True(expected == actual);
         }
 
         [Fact]
         public void GetLetterGradeFTest()
         {
-            var gradeBook = new StandardGradeBook("Test GradeBook", true);
+            var standardGradeBook = (from assembly in AppDomain.CurrentDomain.GetAssemblies()
+                                     from type in assembly.GetTypes()
+                                     where type.Name == "StandardGradeBook"
+                                     select type).FirstOrDefault();
+            if (standardGradeBook == null)
+                throw new Exception("GradeBook.GradeBooks.StandardGradeBook doesn't exist.");
+
+            object gradeBook = Activator.CreateInstance(standardGradeBook, "Test GradeBook", true);
+            MethodInfo method = standardGradeBook.GetMethod("GetLetterGrade");
+
             var expected = 'F';
-            var actual = gradeBook.GetLetterGrade(50);
+            var actual = (char)method.Invoke(gradeBook, new object[] { 50 });
             Assert.True(expected == actual);
         }
     }
