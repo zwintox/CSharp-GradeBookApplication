52c5ec50d80f2d4443ffbc90386178ba5d8543b9
Eric J Fisher <EricJFisher@Live.com>
2017-12-20 12:30:40 -0500

    Corrected two issues that sometimes caused tests to fail incorrectly

--- GradeBookTests/RankedGradingTests.cs
+++ GradeBookTests/RankedGradingTests.cs
@@ -709,6 +709,8 @@ namespace GradeBookTests
             }
             StreamWriter standardOutput = new StreamWriter(Console.OpenStandardOutput());
             Console.SetOut(standardOutput);
+            StreamReader standardInput = new StreamReader(Console.OpenStandardInput());
+            Console.SetIn(standardInput);
 
             // If help command is updated for weighted GPA bypass test
             if (output.Contains("create 'name' 'type' 'weighted' - creates a new gradebook where 'name' is the name of the gradebook, 'type' is what type of grading it should use, and 'weighted' is whether or not grades should be weighted (true or false)."))
